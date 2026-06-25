a9b70bdb49a79e6713cd19d2a334ed49d41baf01
Abhishek <sharma.abhishek.as07@gmail.com>
2026-06-26 00:27:31 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    allowedHosts: ['misterpilot.online', 'www.misterpilot.online'],
   },
   preview: {
     host: '0.0.0.0',
